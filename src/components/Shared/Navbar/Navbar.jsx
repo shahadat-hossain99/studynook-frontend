5471fe011b9c2adc925d7a48181c03e3630fe138
shahadat-hossain99 <shahadathossain31500@gmail.com>
2026-05-22 23:09:58 +0600

    fixed navbar bg-colour issue

--- src/components/Shared/Navbar/Navbar.jsx
+++ src/components/Shared/Navbar/Navbar.jsx
@@ -110,7 +110,7 @@ const Navbar = () => {
   return (
     <>
       {/* NAVBAR */}
-      <nav className="sticky top-0 z-50 border-b border-white/10 bg-[#0B1120]/80 backdrop-blur-xl">
+      <nav className="sticky top-0 z-50 border-b border-white/10 bg-[#0B1120]/90 backdrop-blur-xl">
         <div className="max-w-11/12 mx-auto h-20 px-4 md:px-6 flex items-center justify-between">
           {/* LEFT */}
 
